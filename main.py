7e4c58042a157fbb6d988c440a29fd8003ce473c
Jacobus Burger <therealjacoburger@gmail.com>
2022-05-07 21:49:15 -0700

    added questions

--- main.py
+++ main.py
@@ -2,32 +2,48 @@ from SECRET import TOKEN
 from nextcord.ext import commands
 from nextcord import Intents, Interaction, ButtonStyle, ui
 
-# modified https://github.com/nextcord/nextcord/blob/master/examples/views/confirm.py
-
 
 class Confirm(ui.View):
-    def __init__(self):
+    def __init__(self, Qa, Qb, Qc, Qd, Qi):
         super().__init__(timeout=120)
         self.value = None
+        self.Qa = Qa
+        self.Qb = Qb
+        self.Qc = Qc
+        self.Qd = Qd
+        self.Qi = Qi
 
-    @ui.button(label="Confirm", style=ButtonStyle.green, row=1)
-    async def confirm(self, button: ui.Button, interaction: Interaction):
-        await interaction.send("Confirming", ephemeral=True)
+    @ui.button(label="A", style=ButtonStyle.green, row=1)
+    async def A(self, button: ui.Button, interaction: Interaction):
+        await interaction.send("Answer A", ephemeral=True)
         self.value = True
         for child in self.children:
             child.disabled = True
         await interaction.edit(view=self)
 
-    @ui.button(label="Cancel", style=ButtonStyle.red, row=2)
-    async def cancel(self, button: ui.Button, interaction: Interaction):
-        await interaction.send("Cancelling", ephemeral=True)
+    @ui.button(label="B", style=ButtonStyle.red, row=2)
+    async def B(self, button: ui.Button, interaction: Interaction):
+        await interaction.send("Answer B", ephemeral=True)
         self.value = False
         for child in self.children:
             child.disabled = True
         await interaction.edit(view=self)
 
+    @ui.button(label="C", style=ButtonStyle.grey, row=2)
+    async def C(self, button: ui.Button, interaction: Interaction):
+        await interaction.send("Answer C", ephemeral=True)
+        self.value = False
+        for child in self.children:
+            child.disabled = True
+        await interaction.edit(view=self)
 
-#######################################################################################################
+    @ui.button(label="D", style=ButtonStyle.blurple, row=2)
+    async def D(self, button: ui.Button, interaction: Interaction):
+        await interaction.send("Answer D", ephemeral=True)
+        self.value = False
+        for child in self.children:
+            child.disabled = True
+        await interaction.edit(view=self)
 
 
 intents = Intents.all()
@@ -35,10 +51,10 @@ bot = commands.Bot(command_prefix="$", intents=intents)
 
 
 @bot.command()
-async def btn(ctx):
-    """Asks the user a question to confirm something."""
-    view = Confirm()
-    await ctx.send("Do you want to continue?", view=view)
+async def question(ctx, text, Qa, Qb, Qc, Qd, ans_index):
+    """Asks the user a question"""
+    view = Confirm(Qa, Qb, Qc, Qd, ans_index)
+    await ctx.send(text, view=view)
 
     await view.wait()
     if view.value is None:
@@ -49,4 +65,4 @@ async def btn(ctx):
         print("Cancelled...")
 
 
-bot.run("OTcxODk2Njk1OTMzNzg4MjMx.GWvn7l.e9sDdAFTzRZVdMlcKLcKycOvqO9Z7l3ZY2LDpU")
+bot.run(TOKEN)
